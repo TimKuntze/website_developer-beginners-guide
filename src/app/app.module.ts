8a97f34d419e1c7238fec39609681cd3037e1d36
Tim <67963728+TimKuntze@users.noreply.github.com>
2022-01-25 18:12:07 +0100

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenueComponent } from './menue/menue.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenueComponent
+    MenueComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
